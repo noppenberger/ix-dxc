eb6414f5cb1f195cb429f7d28693b4825a72d312
noppenberger <agithub@npole.de>
2024-11-22 12:55:00 +0100

    content extension

--- src/dxc-administration.tsx
+++ src/dxc-administration.tsx
@@ -1,6 +1,5 @@
 import './styles/application.css';
 
-import { Breakpoint } from '@siemens/ix';
 import {
   IxApplication,
   IxApplicationHeader,
@@ -11,18 +10,19 @@ import {
   IxMenu,
   IxMenuItem,
   IxMenuSettings,
-  IxMenuSettingsItem
+  IxMenuSettingsItem,
+  IxCardList,
+  IxPushCard
 } from '@siemens/ix-react';
 
-import { useState } from 'react';
 
 export default () => {
-  const [breakpoints, setBreakpoints] = useState<Breakpoint[]>(['md']);
-
+  
   return (
-    <IxApplication breakpoints={breakpoints}>
-      <IxApplicationHeader name="Data Xchange DxC Administration">
-
+    <IxApplication>
+      <IxApplicationHeader name="DxC Administration">
+        <div slot="logo">
+          <img className="logo" src="https://ix.siemens.io/img/logo.svg" alt="Siemens Industrial Experience" aria-label="Siemens Industrial Experience"/></div>
         <IxAvatar>
           <IxDropdownItem label="Action 1"></IxDropdownItem>
           <IxDropdownItem label="Action 2"></IxDropdownItem>
@@ -30,8 +30,9 @@ export default () => {
         </IxAvatar>
       </IxApplicationHeader>
       <IxMenu>
+        <IxMenuItem tab-icon="home">Overview</IxMenuItem>
         <IxMenuItem tab-icon="add-application">Add AAS server</IxMenuItem>
-        <IxMenuItem tab-icon="list">Registered AAS servers</IxMenuItem>
+        
         <IxMenuItem tab-icon="consistency-check">Compliance checks</IxMenuItem>
         <IxMenuItem tab-icon="info" slot="bottom">Terms and conditions</IxMenuItem>
         <IxMenuSettings>
@@ -45,8 +46,45 @@ export default () => {
           slot="header"
           header-title="Choose breakpoint"
         ></IxContentHeader>
-        
+        <IxPushCard
+      icon="bulb"
+      notification="99"
+      heading="Heading content"
+      subheading="Subheading"
+      variant="insight"
+    >
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </IxPushCard>
       </IxContent>
+
     </IxApplication>
   );
 };
